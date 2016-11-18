00de5cc2da921a871458ea9ee1daa21bcfd3bd68
Alessandro Portela <aportela119@gmail.com>
2016-11-18 09:21:03 -0500

    test commit

---
+++ TheShareProject/RequestPendingViewController.swift
@@ -0,0 +1,18 @@
+//
+//  RequestPendingViewController.swift
+//  TheShareProject
+//
+//  Created by Alessandro Portela on 11/18/16.
+//  Copyright © 2016 Josh Doman. All rights reserved.
+//
+
+import UIKit
+
+class RequestPendingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
